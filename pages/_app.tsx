5ecfc8598ed5ec1a9553e0989b6f946f5392bce3
fba <fba@cleverreach.com>
2022-11-17 16:34:16 +0100

    Add Inter font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
+import { Inter } from "@next/font/google";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --inter-font: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
