30d22dc14013bff0c5fd457cb946f0f8e985edbf
fba <fba@cleverreach.com>
2023-03-19 21:27:54 +0100

    Remove legacy @next/font dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { classNames } from "../components/utilities";
