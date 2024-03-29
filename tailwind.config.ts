601819bfda3fc6ada6d7353f22eb28c10a1739d0
fba <fba@cleverreach.com>
2024-03-29 13:05:04 +0100

    Add Prettier
    To install prettier, run `npm i -D prettier prettier-plugin-tailwindcss eslint-config-prettier`. Then add a `prettierignore` file based on your `.gitignore` file and a `prettier.config.ts` with the Tailwind CSS plugin enabled. Lastly, add Prettier to the ESLint config file.
    To enable the pre-commit hook, run `npx mrm@2 lint-staged` and adjust the file types for the `lint-staged` option.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('tailwindcss-react-aria-components')
-  ],
+  plugins: [require("tailwindcss-react-aria-components")],
 };
+
 export default config;
